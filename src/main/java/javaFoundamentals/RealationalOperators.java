211299da58339c746b31426d4cf9514ca893f47a
Akshaya HK <akhk4639@gmail.com>
2025-07-31 15:57:01 +0530

    RealationalOperators

---
+++ src/main/java/javaFoundamentals/RealationalOperators.java
@@ -0,0 +1,22 @@
+package javaFoundamentals;
+
+public class RealationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        boolean result1 = a == b;
+        boolean result2 = a != b;
+        boolean result3 = a > b;
+        boolean result4 = a < b;
+        boolean result5 = a >= b;
+        boolean result6 = a <= b;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+    }
+}
