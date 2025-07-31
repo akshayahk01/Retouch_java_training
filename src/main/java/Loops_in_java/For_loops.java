0da124433770ba6cb15085c6c5bc3fe1c999f4cc
Akshaya HK <akhk4639@gmail.com>
2025-07-31 17:10:04 +0530

    LOOPS ARE USED IN JAVA

---
+++ src/main/java/Loops_in_java/For_loops.java
@@ -0,0 +1,34 @@
+package Loops_in_java;
+
+
+//for (initialization; condition; update) {
+//        // code block to execute
+//        }
+
+
+public class For_loops {
+    public static void main(String[] args) {
+
+      /*  just printing the 1-5 numbers */
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println(i);
+                }
+
+        System.out.println("even number");
+        /*  just printing the even numbers */
+        for (int j = 2; j <= 10; j += 2) {
+            System.out.println(j);
+        }
+
+        System.out.println("reverse numbers");
+        /*  just printing the reverse numbers */
+        for (int a = 10; a >= 1; a--) {
+            System.out.println(a);
+        }
+
+
+
+
+
+    }
+}
