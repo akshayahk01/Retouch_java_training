533e7b364f69dc7d7e5c189b3c1dbd2fd215344f
Akshaya HK <akhk4639@gmail.com>
2025-07-31 15:44:00 +0530

    adding operators

--- src/main/java/javaFoundamentals/ArthmaticOperator.java
+++ src/main/java/javaFoundamentals/ArthmaticOperator.java
@@ -1,4 +1,26 @@
 package javaFoundamentals;
 
 public class ArthmaticOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Addition: " + sum);
+
+        // Subtraction
+        int Subtraction = a - b;
+        System.out.println("Subtraction: " + Subtraction);
+
+        // Multiplication
+        int mul = a * b;
+        System.out.println("Multiplication: " + mul);
+
+        // Division
+        int Division = a / b;
+        System.out.println("Division: " + Division);
+
+
+    }
 }
