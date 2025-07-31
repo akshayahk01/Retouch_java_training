d091187bea6cb56cefc7fb469835381994035e64
Akshaya HK <akhk4639@gmail.com>
2025-07-31 16:01:26 +0530

    Conditionaloperator

---
+++ src/main/java/conditionalOperators/WhileLoop.java
@@ -0,0 +1,25 @@
+package conditionalOperators;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+//        increment
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Hi Akshay hk: " + i);
+            i++;
+        }
+// decrement
+        int j = 10;
+        while (j >= 1) {
+            System.out.println("welcome to java: " + j);
+            j--;
+        }
+
+
+        }
+
+
+
+        }
+
+
