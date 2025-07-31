9383be1e096aacfd8e46ce6c647083480f47ec29
Akshaya HK <akhk4639@gmail.com>
2025-07-31 17:09:06 +0530

    Updated Whileloop example

---
+++ src/main/java/Loops_in_java/Whileloop.java
@@ -0,0 +1,25 @@
+package Loops_in_java;
+
+public class Whileloop {
+
+    public static void main(String[] args) {
+
+//        increment
+        int i=1;
+        while (i <=10) {
+            System.out.println("hi Akshay welcome " + i);
+            i++;
+        }
+
+//        decrement
+        System.out.println("in reversed format");
+        int j =10;
+        while (j >=1) {
+            System.out.println("hi Akshay welcome to reverse " + j);
+            j--;
+        }
+
+
+
+    }
+}
