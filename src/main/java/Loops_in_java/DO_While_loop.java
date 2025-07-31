877a6fbcf339b8f3f24def8674b4df9f6e551820
Akshaya HK <akhk4639@gmail.com>
2025-07-31 17:12:32 +0530

    Loops

---
+++ src/main/java/Loops_in_java/DO_While_loop.java
@@ -0,0 +1,37 @@
+package Loops_in_java;
+
+public class DO_While_loop {
+    public static void main(String[] args) {
+        int i =10;
+        do {
+            System.out.println("hi Akshay bro how r u" +i);
+            i++;
+        }
+        while (i==10);
+
+
+// printing the even number
+                int j = 2;
+                do {
+                    System.out.println(j);
+                    j += 2;
+                }
+
+                while (j <= 10);
+
+        System.out.println("ODD NUMBERS");
+//printing odd number
+        int o = 1;
+        do {
+            System.out.println(o);
+
+            o += 3;
+
+        }
+
+        while (o < 10);
+
+            }
+        }
+
+
