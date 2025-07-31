b772271c24a697261856220ac91e1bd02459adae
Akshaya HK <akhk4639@gmail.com>
2025-07-31 16:01:26 +0530

    Typecasting

---
+++ src/main/java/javaFoundamentals/Typecasting.java
@@ -0,0 +1,15 @@
+package javaFoundamentals;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        int a = 100;
+        double b = a; // typecasting int to double
+
+        double x = 99.99;
+        int y = (int) x; //  typecasting double to int
+
+        System.out.println(b);
+        System.out.println(y);
+    }
+}
+
